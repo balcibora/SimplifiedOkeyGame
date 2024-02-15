913f9b2d99fed45385218929f74505af349c8475
Gokdeniz-Derelioglu <derelioglugokdeniz@gmail.com>
2024-02-15 10:36:05 +0300

    Completed Missing Parts in Tile Class
    Filled in the following methods:
    - matchingTiles(Tile t)
    - compareTo(Tile t)
    - canFormChainWith(Tile t)

--- Tile.java
+++ Tile.java
@@ -12,9 +12,11 @@ public class Tile {
     /*
      * TODO: should check if the given tile t and this tile have the same value 
      * return true if they are matching, false otherwise
+     * 
+     * Gökdeniz Derelioğlu - done.
      */
     public boolean matchingTiles(Tile t) {
-        return false;
+        return (this.value == t.value);
     }
 
     /*
@@ -22,9 +24,26 @@ public class Tile {
      * return 1 if given tile has smaller in value
      * return 0 if they have the same value
      * return -1 if the given tile has higher value
+     * 
+     * Gökdeniz Derelioğlu - done.
      */
     public int compareTo(Tile t) {
-       return 0;
+        int output = 2; //terminal value
+
+        if (t.value < this.value)
+        {
+            output = 1;
+        }
+        else if (t.value == this.value)
+        {
+            output = 0;
+        }
+        else
+        {
+            output = -1;
+        }
+       
+        return output;
     }
 
     /*
@@ -32,9 +51,12 @@ public class Tile {
      * this method should check the difference in values of the two tiles
      * should return true if the absoulute value of the difference is 1 (they can form a chain)
      * otherwise, it should return false (they cannot form a chain)
+     * 
+     * Gökdeniz Derelioğlu - done.
      */
     public boolean canFormChainWith(Tile t) {
-        return false;
+        int difference = Math.abs(t.value - this.value);
+        return (difference == 1);
     }
 
     public String toString() {
